15447a88e0454a8f1b48ec053207845a88af24ae
Farhan Rizki <farhanrizki101010@gmail.com>
2023-12-05 03:36:12 +0700

    update

--- hashing.go
+++ hashing.go
@@ -6,9 +6,9 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPass(password string) (string, error) {
-	bytess, err := bcrypt.GenerateFromPassword([]byte(password), 12)
-	return string(bytess), err
+func HashPass(passwordhash string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(passwordhash), 14)
+	return string(bytes), err
 }
 
 func CheckPasswordHash(passwordhash, hash string) bool {
@@ -21,5 +21,3 @@ func CheckEmail(email string) bool {
 	match, _ := regexp.MatchString(emailRegex, email)
 	return match
 }
-
-
